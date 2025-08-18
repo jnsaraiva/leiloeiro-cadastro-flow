b6c32e9a6143a8c4cc655ae21274cb1f4ffb5f3b
jnsaraiva <joaquim.saraiva@gmail.com>
2025-08-18 14:16:06 -0300

    Update RegisterForm.tsx

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -116,7 +116,7 @@ const RegisterForm = () => {
 
       const data: ApiResponse = await response.json();
 console.log(response.json);
-      if (data.status === "sucesso") {
+      if (data.status === "aguardando_verificacao") {
         setMessage({ type: "success", text: "Cadastro realizado com sucesso!" });
         setFormData({ nome_empresa: "", email: "", whatsapp: "", senha: "" });
         toast.success("Cadastro realizado com sucesso!");
