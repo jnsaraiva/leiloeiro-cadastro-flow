e2e29dfd489b4be10afca6f1cbb9af8f06cf954f
jnsaraiva <joaquim.saraiva@gmail.com>
2025-09-01 13:03:32 -0300

    Update RegisterForm.tsx

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -215,7 +215,7 @@ const RegisterForm = () => {
         },
         body: JSON.stringify({
           email: formData.email,
-          otp_code: otpCode,
+          otp_code: formData.otpCode,
         }),
       });
 
