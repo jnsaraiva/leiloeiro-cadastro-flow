1f2c8c4cae35319a6f05625520f1adb766fb93f5
jnsaraiva <joaquim.saraiva@gmail.com>
2025-09-04 16:42:03 -0300

    Update RegisterForm.tsx

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -234,7 +234,7 @@ const RegisterForm = () => {
           setOtpCode("");
           setVerificationMethod(null);
           setMessage({ type: "success", text: "Conta confirmada. Faça seu login!." });
-          navigate('https://dashboard.leilaolovers.com.br');
+          navigate('/dashboard');
         }, 2000);
       } else {
         setOtpMessage({ type: "error", text: "❌ Código inválido. Tente novamente ou reenvie o código." });
